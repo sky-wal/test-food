0a3c2f4fe7fdda5963fd662d8ae61344a7df5f1b
skywal <skywal@skywal.ru>
2022-10-12 12:22:23 +0700

    Lesson 041

--- js/script.js
+++ js/script.js
@@ -48,4 +48,62 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // TIMER
+
+    const deadline = '2022-10-15'; // дата окончания акции
+
+    // функция, вычисляющая разницу между дедлайн и текущим временем
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 *24)), // осталось целых дней
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / (1000 * 60)) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+        
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+
+        };
+    }
+
+    function setClock(selector, endtime) {
+        // Получаем все элементы со страницы
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+        
+        updateClock(); //здесь устанавливаем вызов функции для того, чтобы при рефреше страницы не мигали счетчики
+        
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+        //Вспомогательная функция, чтобы значения все сделать двухзначными
+        function getZero(num) {
+            if (num >= 0 && num < 10) {
+                return `0${num}`;
+            } else {
+                return num;
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
